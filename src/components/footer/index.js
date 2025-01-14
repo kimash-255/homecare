82344dfb9c080864ff4285b1faf7666aa71dcdc7
kimash-255 <rexviscot44@gmail.com>
2025-01-14 21:50:11 +0300

    footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,43 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import servicesData from "@/data/servicesData";
 
 const Footer = () => {
   return (
-    <footer class="main-footer footer-style-one">
-      <div class="bg-pattern-2 bounce-y"></div>
+    <footer className="main-footer footer-style-one">
+      <div className="bg-pattern-2 bounce-y"></div>
 
-      <div class="auto-container">
-        <div class="footer-upper">
-          <div class="row">
-            <div class="footer-logo col-xl-3 col-lg-2">
-              <div class="inner">
-                <div class="logo">
-                  {/* Commented out the logo image and added text */}
-                  <a href="index.html" class="logo-text">
+      <div className="auto-container">
+        <div className="footer-upper">
+          <div className="row">
+            <div className="footer-logo col-xl-3 col-lg-2">
+              <div className="inner">
+                <div className="logo">
+                  <a href="index.html" className="logo-text">
                     Dream Care Homes
                   </a>
                 </div>
               </div>
             </div>
-            <div class="col-xl-7 col-lg-8">
-              <div class="row">
-                <div class="col-md-4 col-sm-6">
-                  <div class="contact-info-block">
-                    <div class="inner">
-                      <i class="icon fa fa-phone-square"></i>
-                      <span class="sub-title">Phone:</span>
-                      <div class="text">
+            <div className="col-xl-7 col-lg-8">
+              <div className="row">
+                <div className="col-md-4 col-sm-6">
+                  <div className="contact-info-block">
+                    <div className="inner">
+                      <i className="icon fa fa-phone-square"></i>
+                      <span className="sub-title">Phone:</span>
+                      <div className="text">
                         <Link href="tel:+12059608084">+1 (205) 960-8084</Link>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                  <div class="contact-info-block">
-                    <div class="inner">
-                      <i class="icon fa fa-envelope"></i>
-                      <span class="sub-title">Email:</span>
-                      <div class="text">
+                <div className="col-md-4 col-sm-6">
+                  <div className="contact-info-block">
+                    <div className="inner">
+                      <i className="icon fa fa-envelope"></i>
+                      <span className="sub-title">Email:</span>
+                      <div className="text">
                         <Link href="mailto:dreamcarehomes@yahoo.com">
                           dreamcarehomes@yahoo.com
                         </Link>
@@ -45,12 +45,12 @@ const Footer = () => {
                     </div>
                   </div>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                  <div class="contact-info-block before-none">
-                    <div class="inner">
-                      <i class="icon fa fa-map-marker"></i>
-                      <span class="sub-title">Our Location:</span>
-                      <div class="text">
+                <div className="col-md-4 col-sm-6">
+                  <div className="contact-info-block before-none">
+                    <div className="inner">
+                      <i className="icon fa fa-map-marker"></i>
+                      <span className="sub-title">Our Location:</span>
+                      <div className="text">
                         <p>
                           <strong>Dream Care Homes LLC</strong>
                         </p>
@@ -63,8 +63,8 @@ const Footer = () => {
               </div>
             </div>
 
-            <div class="contact-info col-lg-2">
-              <div class="inner">
+            <div className="contact-info col-lg-2">
+              <div className="inner">
                 <Link href="contact" className="theme-btn btn-style-three">
                   <span>Get in Touch</span>
                 </Link>
@@ -74,93 +74,49 @@ const Footer = () => {
         </div>
       </div>
 
-      <div class="widgets-section">
-        <div class="auto-container">
-          <div class="row align-items-center">
-            <div class="footer-column pe-0 col-xl-3 col-lg-4 col-md-6 col-sm-12">
-              <div class="footer-widget about-widget">
-                <div class="widget-content">
-                  <div class="footer-abvout-icon-box">
-                    <div class="inner-box pr-10">
-                      <div class="icon flex-shrink-0">
-                        <i class="far fa-clock-rotate-left"></i>
+      <div className="widgets-section">
+        <div className="auto-container">
+          <div className="row align-items-center">
+            <div className="footer-column pe-0 col-xl-3 col-lg-4 col-md-6 col-sm-12">
+              <div className="footer-widget about-widget">
+                <div className="widget-content">
+                  <div className="footer-abvout-icon-box">
+                    <div className="inner-box pr-10">
+                      <div className="icon flex-shrink-0">
+                        <i className="far fa-clock-rotate-left"></i>
                       </div>
-                      <div class="inner">
-                        <h5 class="title">Opening Hours</h5>
-                        <div class="time">Mon-Sat(8.00-6.00) Sunday-Closed</div>
+                      <div className="inner">
+                        <h5 className="title">Opening Hours</h5>
+                        <div className="time">
+                          Mon-Sat(8.00-6.00) Sunday-Closed
+                        </div>
                       </div>
                     </div>
                   </div>
 
-                  <div class="text">
+                  <div className="text">
                     Dream Care Homes LLC provides compassionate care for
                     individuals with developmental disabilities.
                   </div>
-                  <ul class="social-icon-four">
+                  <ul className="social-icon-four">
                     <li>
                       <Link href="#">
-                        <i class="fab fa-facebook-f"></i>
+                        <i className="fab fa-facebook-f"></i>
                       </Link>
                     </li>
                     <li>
                       <Link href="#">
-                        <i class="fab fa-twitter"></i>
+                        <i className="fab fa-twitter"></i>
                       </Link>
                     </li>
                     <li>
                       <Link href="#">
-                        <i class="fab fa-instagram"></i>
+                        <i className="fab fa-instagram"></i>
                       </Link>
                     </li>
                     <li>
                       <Link href="#">
-                        <i class="fab fa-pinterest"></i>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div class="footer-column col-lg-2 offset-xl-1 col-md-6 col-sm-6">
-              <div class="footer-widget links-widget">
-                <h5 class="widget-title">Types of Services</h5>
-                <div class="widget-content">
-                  <ul class="user-links">
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i> Safe
-                        Transportation
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i>{" "}
-                        Respite Care
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i> 24/7
-                        Support
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i>{" "}
-                        Support Planning
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i>{" "}
-                        Medical Support
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i>{" "}
-                        Vocational Training
+                        <i className="fab fa-pinterest"></i>
                       </Link>
                     </li>
                   </ul>
@@ -168,27 +124,52 @@ const Footer = () => {
               </div>
             </div>
 
-            <div class="footer-column col-xl-3 col-lg-2 col-md-6 col-sm-6">
-              <div class="footer-widget links-widget-2">
-                <h5 class="widget-title">Explore</h5>
-                <div class="widget-content">
-                  <ul class="user-links">
+            <div className="footer-column col-lg-2 offset-xl-1 col-md-6 col-sm-6">
+              <div className="footer-widget links-widget">
+                <h5 className="widget-title">Types of Services</h5>
+                <div className="widget-content">
+                  <ul className="user-links">
+                    <li className="dropdown">
+                      <Link href="/services">
+                        {/* Remove the "Services" from the links */}
+                      </Link>
+                      <ul>
+                        {servicesData.map((service) => (
+                          <li key={service.slug}>
+                            <Link href={`/services/${service.slug}`}>
+                              {service.title.split(" ")[0]}{" "}
+                              {service.title.split(" ").slice(-1)}
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="footer-column col-xl-3 col-lg-2 col-md-6 col-sm-6">
+              <div className="footer-widget links-widget-2">
+                <h5 className="widget-title">Explore</h5>
+                <div className="widget-content">
+                  <ul className="user-links">
                     <li>
                       <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i> About
-                        Us
+                        <i className="arrow-icon far fa-window-minimize"></i>{" "}
+                        About Us
                       </Link>
                     </li>
                     <li>
                       <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i>{" "}
+                        <i className="arrow-icon far fa-window-minimize"></i>{" "}
                         Services
                       </Link>
                     </li>
                     <li>
                       <Link href="#">
-                        <i class="arrow-icon far fa-window-minimize"></i> Latest
-                        News
+                        <i className="arrow-icon far fa-window-minimize"></i>{" "}
+                        Latest News
                       </Link>
                     </li>
                   </ul>
@@ -196,55 +177,55 @@ const Footer = () => {
               </div>
             </div>
 
-            <div class="footer-column col-xl-3 col-lg-4 col-md-6 col-sm-6">
-              <div class="footer-widget blog-widget">
-                <h5 class="widget-title">Latest news</h5>
-                <div class="widget-content">
-                  <article class="recent-post">
-                    <div class="inner">
-                      <figure class="post-thumb">
+            <div className="footer-column col-xl-3 col-lg-4 col-md-6 col-sm-6">
+              <div className="footer-widget blog-widget">
+                <h5 className="widget-title">Latest news</h5>
+                <div className="widget-content">
+                  <article className="recent-post">
+                    <div className="inner">
+                      <figure className="post-thumb">
                         <Link href="#">
                           <img src="images/resource/post-thumb-1.jpg" alt="" />
                         </Link>
                       </figure>
-                      <div class="post-info">
-                        <i class="fas fa-calendar-alt"></i> January 11, 2024
+                      <div className="post-info">
+                        <i className="fas fa-calendar-alt"></i> January 11, 2024
                       </div>
-                      <h6 class="post-title">
+                      <h6 className="post-title">
                         <Link href="#">
                           New Caring Thoughts You Can Fundraise
                         </Link>
                       </h6>
                     </div>
                   </article>
-                  <article class="recent-post">
-                    <div class="inner">
-                      <figure class="post-thumb">
+                  <article className="recent-post">
+                    <div className="inner">
+                      <figure className="post-thumb">
                         <Link href="#">
                           <img src="images/resource/post-thumb-2.jpg" alt="" />
                         </Link>
                       </figure>
-                      <div class="post-info">
-                        <i class="fas fa-calendar-alt"></i> January 11, 2024
+                      <div className="post-info">
+                        <i className="fas fa-calendar-alt"></i> January 11, 2024
                       </div>
-                      <h6 class="post-title">
+                      <h6 className="post-title">
                         <Link href="#">
                           Five Tips to Improve Seniors’ Quality of Life
                         </Link>
                       </h6>
                     </div>
                   </article>
-                  <article class="recent-post">
-                    <div class="inner">
-                      <figure class="post-thumb">
+                  <article className="recent-post">
+                    <div className="inner">
+                      <figure className="post-thumb">
                         <Link href="#">
                           <img src="images/resource/post-thumb-3.jpg" alt="" />
                         </Link>
                       </figure>
-                      <div class="post-info">
-                        <i class="fas fa-calendar-alt"></i> January 11, 2024
+                      <div className="post-info">
+                        <i className="fas fa-calendar-alt"></i> January 11, 2024
                       </div>
-                      <h6 class="post-title">
+                      <h6 className="post-title">
                         <Link href="#">
                           Exploring your In-home Care Support Options
                         </Link>
@@ -258,10 +239,10 @@ const Footer = () => {
         </div>
       </div>
 
-      <div class="footer-bottom">
-        <div class="auto-container">
-          <div class="inner-container">
-            <div class="copyright-text">
+      <div className="footer-bottom">
+        <div className="auto-container">
+          <div className="inner-container">
+            <div className="copyright-text">
               © Copyright 2024 by Dream Care Homes LLC
             </div>
           </div>
